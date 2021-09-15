2e76e691b658fb728985a16f30e3b918b8862d00
abdulsalamalmahdi <abdulsalamalamhadi@gmail.com>
2021-09-15 10:57:18 +0200

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Picker from '../pages/shoe'
 
 export default function Home() {
   return (
@@ -64,6 +65,7 @@ export default function Home() {
           </span>
         </a>
       </footer>
+      <Picker className={styles.logo}></Picker>
     </div>
   )
 }
