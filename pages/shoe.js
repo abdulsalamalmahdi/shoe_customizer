a7c4aaf86908df24f8cb716c6986032d40d7a422
abdulsalamalmahdi <abdulsalamalamhadi@gmail.com>
2021-09-19 13:35:40 +0200

    fixing style

--- pages/shoe.js
+++ pages/shoe.js
@@ -22,12 +22,13 @@ const state = proxy({
 
 function Shoe() {
   const ref = useRef()
+ 
   const snap = useSnapshot(state)
   // Drei's useGLTF hook sets up draco automatically, that's how it differs from useLoader(GLTFLoader, url)
   // { nodes, materials } are extras that come from useLoader, these do not exist in threejs/GLTFLoader
   // nodes is a named collection of meshes, materials a named collection of materials
   
-//   const { nodes, materials } = useGLTF("shoe-draco.glb")
+  const { nodes, materials } = useGLTF("shoe.glb")
 
   // Animate model
   useFrame((state) => {
@@ -36,6 +37,7 @@ function Shoe() {
     ref.current.rotation.x = Math.cos(t / 4) / 8
     ref.current.rotation.y = Math.sin(t / 4) / 8
     ref.current.position.y = (1 + Math.sin(t / 1.5)) / 10
+  
   })
 
   // Cursor showing current color
@@ -47,24 +49,24 @@ function Shoe() {
   }, [hovered])
 
   // Using the GLTFJSX output here to wire in app-state and hook up events
-//   return (
-//     <group
-//       ref={ref}
-//       dispose={null}
-//       onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
-//       onPointerOut={(e) => e.intersections.length === 0 && set(null)}
-//       onPointerMissed={() => (state.current = null)}
-//       onPointerDown={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
-//       <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
-//       <mesh receiveShadow castShadow geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
-//     </group>
-//   )
+  return (
+    <group
+      ref={ref}
+      dispose={null}
+      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+      onPointerMissed={() => (state.current = null)}
+      onPointerDown={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+      <mesh receiveShadow castShadow geometry={nodes.shoe.geometry} material={materials.laces} material-color={snap.items.laces} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_1.geometry} material={materials.mesh} material-color={snap.items.mesh} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_2.geometry} material={materials.caps} material-color={snap.items.caps} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_3.geometry} material={materials.inner} material-color={snap.items.inner} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_4.geometry} material={materials.sole} material-color={snap.items.sole} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_5.geometry} material={materials.stripes} material-color={snap.items.stripes} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_6.geometry} material={materials.band} material-color={snap.items.band} />
+      <mesh receiveShadow castShadow geometry={nodes.shoe_7.geometry} material={materials.patch} material-color={snap.items.patch} />
+    </group>
+  )
 }
 
 function Picker() {
@@ -80,7 +82,7 @@ function Picker() {
 export default function App() {
   return (
     <>
-      {/* <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+      <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
         <ambientLight intensity={0.7} />
         <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
         <Suspense fallback={null}>
@@ -89,7 +91,7 @@ export default function App() {
           <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={1.5} far={0.8} />
         </Suspense>
         <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
-      </Canvas> */}
+      </Canvas>
       <Picker />
     </>
   )
